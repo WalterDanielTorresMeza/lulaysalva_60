d8be0801f1ec18e866e34644c6454f3a546a0fe2
WalterDanielTorresMeza <38113701+WalterDanielTorresMeza@users.noreply.github.com>
2025-06-15 19:43:21 -0600

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import './Header.css';
 const Header = () => (
   <header className="header">
     <div className="header-content">
-      <h1 className="header-title">Lula y Salva</h1>
+      <h1 className="header-title">Lula y Salvador</h1>
       <p className="header-subtitle">
         Acompáñanos a celebrar la alegría de cumplir <br/>
         60 años de vida
