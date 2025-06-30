346e69c3fbfeb70b57cfc2c7e1537e23abc68eda
WalterDanielTorresMeza <38113701+WalterDanielTorresMeza@users.noreply.github.com>
2025-06-29 20:10:30 -0600

    Update Iglesia.js

--- src/components/Iglesia.js
+++ src/components/Iglesia.js
@@ -11,24 +11,24 @@ const EventLocationI = () => {
 
   return (
     <section className="event-location" data-aos="fade-up">
-      <h2 className="location-title">Lugar del Evento</h2>
+      <h2 className="location-title">Misa</h2>
       {/* Imagen del lugar */}
       <div className="location-image-container">
         <img
           src="/quinta.jpg"
-          alt="Salón Estrella Dorada"
+          alt="Parroquia San Antonio de Padua"
           className="location-image"
         />
       </div>
       <p className="location-address">
-        Quinta de Leonarda<br />
-        Priv. San José 111, Morelos, Zacatecas <br />
+        Parroquia San Antonio de Padua<br />
+        Belén 2, Zona Centro, 98100 Morelos, Zacatecas <br />
         26 de Julio de 2025
       </p>
       {/* Botón para abrir Google Maps */}
       <div className="location-button-container">
         <a
-          href="https://www.google.com/maps/place/Quinta+de+Leonarda/@22.8642751,-102.6087338,17z/data=!3m1!4b1!4m6!3m5!1s0x86824dab1c6c6bc3:0x534011502e8387bd!8m2!3d22.8642702!4d-102.6061589!16s%2Fg%2F11hz9cyytg?entry=ttu&g_ep=EgoyMDI1MDYxMS4wIKXMDSoASAFQAw%3D%3D"
+          href="https://www.google.com/maps/place/Parroquia+San+Antonio+de+Padua/@22.8621504,-102.6107111,18z/data=!4m6!3m5!1s0x86824d4fe389d1cb:0xf72ee9b45a630470!8m2!3d22.8619675!4d-102.6107621!16s%2Fg%2F1tj530hr?entry=ttu&g_ep=EgoyMDI1MDYyMy4yIKXMDSoASAFQAw%3D%3D"
           target="_blank"
           rel="noopener noreferrer"
           className="location-button"
