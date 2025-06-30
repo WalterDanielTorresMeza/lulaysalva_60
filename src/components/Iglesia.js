0e8ee1f2a588b8e9eb63e4ee212c5032972509e1
WalterDanielTorresMeza <38113701+WalterDanielTorresMeza@users.noreply.github.com>
2025-06-29 20:25:18 -0600

    Update Iglesia.js

--- src/components/Iglesia.js
+++ src/components/Iglesia.js
@@ -22,13 +22,13 @@ const EventLocationI = () => {
       </div>
       <p className="location-address">
         Parroquia San Antonio de Padua<br />
-        Belén 2, Zona Centro, 98100 Morelos, Zacatecas <br />
+        Av San Antonio 70, Zona Centro, 98100 Morelos, Zacatecas <br />
         26 de Julio de 2025
       </p>
       {/* Botón para abrir Google Maps */}
       <div className="location-button-container">
         <a
-          href="https://www.google.com/maps/place/Parroquia+San+Antonio+de+Padua/@22.8621504,-102.6107111,18z/data=!4m6!3m5!1s0x86824d4fe389d1cb:0xf72ee9b45a630470!8m2!3d22.8619675!4d-102.6107621!16s%2Fg%2F1tj530hr?entry=ttu&g_ep=EgoyMDI1MDYyMy4yIKXMDSoASAFQAw%3D%3D"
+          href="https://www.google.com/maps/place/San+Antonio+70,+Zona+Centro,+98100+Morelos,+Zac./@22.8616671,-102.6114609,19z/data=!3m1!4b1!4m6!3m5!1s0x86824d5b2fdfc25f:0xddb543f886a76d40!8m2!3d22.8616671!4d-102.6108158!16s%2Fg%2F11c2w5z621?entry=ttu&g_ep=EgoyMDI1MDYyMy4yIKXMDSoASAFQAw%3D%3D"
           target="_blank"
           rel="noopener noreferrer"
           className="location-button"
